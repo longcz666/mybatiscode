e1cf4f30c605ecb3f7a473de90fb96806ee3b432
longcz <2598635373@qq.com>
2022-10-02 20:48:13 +0800

    提交的代码2。。。

---
+++ src/test/java/com/blb/demo01/CRUDTest.java
@@ -0,0 +1,13 @@
+package com.blb.demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CRUDTest {
+
+    @Test
+    void test01(){
+
+    }
+}
